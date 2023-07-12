83476e88314cbe21f6e8833e344b13b305ef0c87
Pedro26fer <p2698fernandes@gmail.com>
2023-07-11 23:02:02 -0300

    feat: some components of header

--- my_pokedex/src/pages/index.tsx
+++ my_pokedex/src/pages/index.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import {Header} from "../components/Header"
+import { SearchBar } from '@/components/Search'
+import { useEffect, useState } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between `}
     >
-      <Header/>
+      <Header>
+        <SearchBar/>
+      </Header>
       
    
    </main>
