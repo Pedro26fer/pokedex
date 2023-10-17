3aa3139f0bf850d212463e85da40c7efb7e7d415
Pedro26fer <p2698fernandes@gmail.com>
2023-10-17 19:03:32 -0300

    feat: .env

--- my_pokedex/src/pages/_app.tsx
+++ my_pokedex/src/pages/_app.tsx
@@ -1,19 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-    <>
-       <Script 
-            id="9094217428" 
-            async
-            onError={(e) => { console.error("Script failed to load", e);}}
-            strategy="afterInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-987************676"
-            crossOrigin="anonymous"
-          />
-          <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
